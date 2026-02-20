af6758a0d500bd2cb1c0e85574bb89de22d3828d
William Markezana <william.markezana@gmail.com>
2026-02-20 12:12:58 -0500

    bflb: add BL70xL BLE controller and RF binary blobs and headers
    Signed-off-by: William Markezana <william.markezana@gmail.com>

---
+++ include/bouffalolab/bl70xl/rf/bl702l_rf.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2016-2022 Bouffalolab.
+ *
+ * This file is part of
+ *     *** Bouffalolab Software Dev Kit ***
+ *      (see www.bouffalolab.com).
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of Bouffalo Lab nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef _BL702L_RF_PUBLIC_H_
+#define _BL702L_RF_PUBLIC_H_
+
+uint32_t rf_set_init_tsen_value(int16_t tsen_value);
+uint32_t rf_inc_cal_tsen_based(int16_t tsen_value);
+
+/* callback function, should be implemented by user */
+void rf_reset_done_callback(void);
+void rf_full_cal_start_callback(uint32_t addr, uint32_t size);
+
+/* RF API */
+enum {
+	MODE_BLE_ONLY = 0,
+	MODE_ZB_ONLY = 1,
+	MODE_BZ_COEX = 2,
+};
+
+void rf_set_bz_mode(uint8_t mode);
+void rf_set_tx_bypass(uint8_t en);
+uint8_t rf_get_rf_state(void);
+
+#endif
