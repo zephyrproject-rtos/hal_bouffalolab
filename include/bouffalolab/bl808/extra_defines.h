90cfc35027eaeed4c4cf03a269143e819069ba03
William Markezana <william.markezana@gmail.com>
2026-02-23 18:52:16 -0500

    bflb: Add register files for bl808 series
    Signed-off-by: William Markezana <william.markezana@gmail.com>

---
+++ include/bouffalolab/bl808/extra_defines.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2025 Bouffalo lab
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+ 
+#define BFLB_EF_CTRL_BASE ((uint32_t)0x20056000)
+
+#define EF_CTRL_EFUSE_CYCLE_PROTECT (0x0)
+#define EF_CTRL_EFUSE_CTRL_PROTECT  (0xbf << 8)
+#define EF_CTRL_OP_MODE_AUTO        0
+#define EF_CTRL_PARA_DFT            0
+
+#define EF_CTRL_DFT_TIMEOUT_VAL (160 * 1000)
+
+#define EF_CTRL_EFUSE_R0_SIZE 512
+
