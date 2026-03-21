4607d5643da1665c2ff0a46bdfe51ad080283507
William Markezana <william.markezana@gmail.com>
2026-03-21 10:16:26 -0400

    bflb: add BL61x WiFi MACSW headers and binary blobs
    Add WiFi MAC software (MACSW) and WL80211 headers for BL61x
    including MAC frame definitions, PHY interface,
    coexistence manager.
    
    Binary blobs sourced from bouffalo_sdk v2.3.26.
    
    Signed-off-by: William Markezana <william.markezana@gmail.com>

---
+++ include/bouffalolab/bl61x/wifi/macsw_bridge_config.h
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2016-2026 Bouffalolab.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifdef __MACSW_CONFIG_INTERNEL_ALLOW__
+#define CFG_AMSDU_8K
+#define CFG_TXDESC0 12
+#define CFG_TXDESC1 64
+#define CFG_TXDESC2 12
+#define CFG_TXDESC3 12
+#define CFG_TXDESC4 2
+#endif
+
+#undef CFG_BARX
+#undef CFG_BATX
+#undef CFG_REORD_BUF
+#define CFG_BARX 12
+#define CFG_BATX 12
+#define CFG_REORD_BUF 12
---
+++ include/bouffalolab/bl61x/wifi/wl80211_mac.h
@@ -0,0 +1,186 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2016-2026 Bouffalolab.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef _WL80211_MAC_H
+#define _WL80211_MAC_H
+
+/* global variable */
+extern int8_t wl80211_mac_vif[WL80211_VIF_MAX];
+
+struct macsw_tx_desc {
+    /* Information provided by Host */
+    struct hostdesc host;
+#if (!MACSW_UMAC_PRESENT)
+    /* Information provided by UMAC */
+    struct umacdesc umac;
+#endif
+
+    /* TX HW descriptor attached to the MPDU */
+    /* struct tx_hw_desc hw_desc; */
+    uint32_t hw_desc[STRUCT_SIZE_TX_HW_DESC / 4];
+    /* Buffer header structure - Must be the last element of the structure as it */
+    /* shall be placed just before the payload space */
+    /* struct txl_buffer_tag buffer; */
+    uint32_t buffer[STRUCT_SIZE_TXL_BUFFER_TAG / 4];
+};
+
+struct wl80211_mac_tx_desc {
+    STAILQ_ENTRY(wl80211_mac_tx_desc) entries;
+
+    /* tx done callback */
+    void (*txdone_cb)(void *);
+    void *opaque;
+
+    /* driver specified descriptor (macsw) */
+    struct macsw_tx_desc txdesc;
+    /* TODO struct net80211 descriptor */
+};
+
+struct wl80211_mac_rx_desc {
+    STAILQ_ENTRY(wl80211_mac_rx_desc) entries;
+    /* Structure containing the information about the received payload - Payload */
+    /* must be just after !!! */
+    struct rx_info info;
+    /* Payload buffer space - must be after rx_info  !!! */
+    uint32_t payload[];
+};
+
+int wl80211_mac_rx_buffer_init(size_t payload_len);
+
+int wl80211_mac_mgmt_input(uint8_t vif, uint8_t *payload, size_t len);
+int wl80211_mac_disconnect(uint16_t reason_code, uint16_t status_code);
+void wl80211_mac_sta_deauth_handler(uint8_t *payload, uint32_t len);
+void wl80211_mac_sta_disassoc_handler(uint8_t *payload, uint32_t len);
+
+/**
+ * Start WiFi scan (internal MAC layer function)
+ *
+ * Return value:
+ *   0  : Success
+ *   -1 : Memory allocation failed (malloc scan ops or scan req failed)
+ */
+int wl80211_mac_do_scan(struct wl80211_scan_params *params);
+
+/**
+ * Start WiFi connection (internal MAC layer function)
+ *
+ * Return value:
+ *   0  : Success
+ *   -1 : Memory allocation failed (malloc ctx or connect_params failed)
+ */
+int wl80211_mac_do_connect(struct wl80211_connect_params *conn_params);
+
+int wl80211_mac_set_key(uint8_t vif_idx, uint8_t sta_idx, uint8_t key_idx, uint8_t *key, uint8_t key_len,
+                        uint8_t cipher_suite, uint8_t spp, bool pairwise);
+int wl80211_mac_clr_ptk(uint8_t vif_idx, uint8_t sta_idx);
+int wl80211_mac_clr_gtk(uint8_t vif_idx, uint8_t sta_idx);
+
+int wl80211_mac_ctrl_port(uint8_t sta_id, int control_port_open);
+int wl80211_mac_set_ps_mode(int enable);
+int wl80211_mac_set_rate(const struct wl80211_rate_config *cfg);
+int wl80211_mac_chan_config_update(uint8_t channel24G_num, const uint8_t *channel24G_chan, uint8_t channel5G_num,
+                                   const uint8_t *channel5G_chan);
+struct mac_chan_def *wl80211_mac_chan_get(int freq);
+
+/**
+ * Start AP mode (internal MAC layer function)
+ *
+ * Return value:
+ *   0  : Success
+ *   -1 : Failed - invalid center_freq1
+ *   -2 : Failed - memory allocation failed
+ *   -3 : Failed - IE length too long
+ */
+int wl80211_mac_start_ap(struct wl80211_ap_settings *ap_settings);
+
+/**
+ * Stop AP mode (internal MAC layer function)
+ *
+ * Return value: 0 on success
+ */
+int wl80211_mac_stop_ap(void);
+int wl80211_mac_ap_del_sta(uint8_t sta_idx);
+/* AP mode process auth packet */
+void wl80211_mac_ap_auth_handler(void *frame_payload, uint32_t frame_length);
+/* AP mode process assoc packet */
+void wl80211_mac_ap_assoc_req_handler(void *frame_payload, uint32_t frame_length, bool is_reassoc);
+/* AP mode process probe request packet */
+void wl80211_mac_ap_probe_req_handler(void *frame_payload, uint32_t frame_length);
+/* AP mode process deauth packet */
+void wl80211_mac_ap_deauth_handler(void *frame_payload, uint32_t frame_length);
+/* AP mode process disassoc packet */
+void wl80211_mac_ap_disassoc_handler(void *frame_payload, uint32_t frame_length);
+
+extern wl80211_monitor_rx_cb_t _wl80211_monitor_rx;
+extern void *_wl80211_monitor_rx_ctx;
+
+/**
+ * Start monitor mode (internal MAC layer function)
+ *
+ * Return value:
+ *   0  : Success
+ *   -1 : Failed - mon_setting is NULL
+ *   -2 : Failed - recv callback is NULL
+ *   -3 : Failed - invalid center_freq1
+ */
+int wl80211_mac_monitor_start(struct wl80211_monitor_settings *mon_setting);
+
+/**
+ * Stop monitor mode (internal MAC layer function)
+ *
+ * Return value: 0 on success
+ */
+int wl80211_mac_monitor_stop(void);
+
+/**
+ * Inject raw 802.11 frame (internal implementation)
+ *
+ * @param params Frame injection parameters
+ * @return 0 on success, negative value on error
+ */
+int wl80211_mac_inject_frame(struct wl80211_inject_frame_params *params);
+
+/* only for type checker */
+struct wl80211_tx_header;
+#define WL80211_MAC_TX_FLAG_MGMT 0x1
+int wl80211_mac_tx(uint8_t vif_type, struct wl80211_tx_header *desc, unsigned int flags, struct iovec *seg, int seg_cnt,
+                   void *txdone_cb, void *opaque);
+
+void wl80211_mac_rx_free(void *info);
+
+/**********************/
+/* 驱动专用接口和变量 */
+/* Utils helper function */
+/**********************/
+extern const struct me_chan_config_req _macsw_chan_def;
+
+/**
+ * Add VIF (internal function)
+ *
+ * Return value:
+ *   >=0 : VIF index
+ *   -1  : Failed (vif_mgmt_register failed, see vif_mgmt.c for detailed error codes)
+ */
+int _macsw_add_vif(uint8_t type, uint8_t mac[6]);
+
+/**
+ * Remove VIF (internal function)
+ *
+ * Return value: VIF index on success
+ */
+int _macsw_remove_vif(int vif_idx);
+
+struct scanu_start_req *_macsw_make_scan_req(struct wl80211_scan_params *params);
+struct me_config_req *_macsw_make_me_config(void);
+void _find_wpa_rsn_ie(uint32_t var_part_addr, uint32_t var_part_len, uint32_t *wpa_ie, uint32_t *rsn_ie);
+uint8_t _macsw_get_staid(int vif_idx, struct mac_addr *dst_addr, bool mgmt_frame);
+void _sme_auth_mgmt_rx(uint8_t *frame, size_t len);
+void *_ap_get_wpa_sm(uint8_t *mac);
+
+int _external_auth_ind(ke_msg_id_t const msgid, void *param, ke_task_id_t const dest_id, ke_task_id_t const src_id);
+int _ap_start_cfm(ke_msg_id_t const msgid, void *param, ke_task_id_t const dest_id, ke_task_id_t const src_id);
+int _sta_add_cfm(ke_msg_id_t const msgid, void *param, ke_task_id_t const dest_id, ke_task_id_t const src_id);
+int _credits_update_ind(ke_msg_id_t const msgid, void *param, ke_task_id_t const dest_id, ke_task_id_t const src_id);
+#endif
