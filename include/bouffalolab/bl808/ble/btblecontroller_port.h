ed275a2624e0f171c42e83e0d4c2ca69ca37674f
William Markezana <william.markezana@gmail.com>
2026-07-07 12:57:56 -0400

    bflb: add BL808 RF controller blobs and headers
    Add BLE controller, WiFi, and PHY/RF API headers for BL808. The
    corresponding binary blobs (BLE controller, WiFi MAC firmware and
    PHY/RF libraries) are referenced in module.yml and fetched with west
    blobs from the pine64 pinevoice_smartspeaker_sdk repository.
    
    Signed-off-by: William Markezana <william.markezana@gmail.com>
    
    # Conflicts:
    #	zephyr/module.yml

---
+++ include/bouffalolab/bl808/ble/btblecontroller_port.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2016-2026 Bouffalolab.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is part of
+ *     *** Bouffalolab Software Dev Kit ***
+ *      (see www.bouffalolab.com).
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of Bouffalo Lab nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef BTBLECONTROLLER_PORT_API_H_
+#define BTBLECONTROLLER_PORT_API_H_
+
+void btblecontroller_ble_irq_init(void *handler);
+void btblecontroller_bt_irq_init(void *handler);
+void btblecontroller_dm_irq_init(void *handler);
+void btblecontroller_ble_irq_enable(uint8_t enable);
+void btblecontroller_bt_irq_enable(uint8_t enable);
+void btblecontroller_dm_irq_enable(uint8_t enable);
+void btblecontroller_enable_ble_clk(uint8_t enable);
+void btblecontroller_rf_restore(void);
+int btblecontroller_efuse_read_mac(uint8_t mac[6]);
+void btblecontroller_software_btdm_reset(void);
+void btblecontroller_software_pds_reset(void);
+void btblecontroller_pds_trim_rc32m(void);
+uint8_t btblecontrolller_get_chip_version(void);
+void btblecontroller_sys_reset(void);
+int btblecontroller_putchar(int c);
+void btblecontroller_puts(const char *str);
+
+#endif /* BTBLECONTROLLER_PORT_API_H_ */
